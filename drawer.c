87fb1b103435e3c5cd7666fea3132999b449e438
Gallien BOUDRIE <gboudrie@e1r12p9.42.fr>
2016-04-25 19:30:55 +0200

    efsffs

--- drawer.c
+++ drawer.c
@@ -6,7 +6,7 @@
 /*   By: gboudrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/15 17:37:25 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/04/11 21:07:33 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/04/25 19:30:16 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,27 @@
 
 t_dot				convert(int x, int y, int z, t_dot dot)
 {
-	dot.x = (x * 4 - 0.3 * z) + SIZE_X / 10 - 0.4 * y;
-	dot.y = (y * 3 - (0.3 / 2) * z) + SIZE_Y / 5 - 0.8 * x;
-	dot.color = (z > 0 ? 0x0008FF08 : 0x000088F8);
+	int		wide_x;
+	int		wide_y;
+	double	decal_x;
+	double	decal_y;
+	double	height;
+
+	wide_x = 8 *15;
+	wide_y = 5 *15;
+	decal_x = 0.4 *15;
+	decal_y = 0.3 *15;
+	height = 0.6 *15;
+	dot.x = (x * wide_x - height * z) + SIZE_X / 20 - decal_x * y;
+	dot.y = (y * wide_y - (height / 2) * z) + SIZE_Y / 15 - decal_y * x;
+	dot.color = (z > 0 ? 0x0000FF00 : 0x00FFFFFF);
 	return (dot);
 }
 
 void				img_addr(t_env env, int x, int y, int color)
 {
-	if (!(x > env.siz || x < 0 || y < 0
-		  || x > SIZE_X || y > SIZE_Y))
+	if (!(x > env.siz - 1 || x < 0 || y < 0
+		  || x > SIZE_X - 1 || y > SIZE_Y - 1))
 		ft_memcpy(&env.img[(x - 1) * 4 + (y - 1) * env.siz], &color, 4);
 }
 
@@ -58,42 +69,47 @@ void				drawer(t_env env)
 	}
 }
 
-/*int					color_add(t_dot a, t_dot b)
+static int			color_add(t_dot a, t_dot b)
 {
-	int		diff;
-	char	*ptra;
-	char	*ptrb;
-	char	*ptr;
-	int		res;
+	int				diff;
+	unsigned char	*ptra;
+	unsigned char	*ptrb;
+	unsigned char	tab[4];
+	int				res;
 
+	if (a.color == b.color)
+		return (0);
 	diff = (b.x - a.x > b.y - a.y ? b.x - a.x : b.y - a.y);
-	*ptra = a.color;
-	*ptrb = b.color;
-	res = (ptra[3] > ptrb[3] ? ptra[3] - ptrb[3] : ptrb[3] - ptra[3]);
-	res += (ptra[2] > ptrb[2] ? ptra[2] - ptrb[2] : ptrb[2] - ptra[2]) * 256;
-	res += (ptra[1] > ptrb[1] ? ptra[1] - ptrb[1] : ptrb[1] - ptra[1]) * 256 * 256;
-	res += (ptra[0] > ptrb[0] ? ptra[0] - ptrb[0] : ptrb[0] - ptra[0]) * 256 * 256 * 256;
-	res /= diff;
+	if (diff == 0)
+		return (0);
+	ptra = (unsigned char *)&a.color;
+	ptrb = (unsigned char *)&b.color;
+	tab[0] = (ptrb[0] - ptra[0]) / diff;
+	tab[1] = (ptrb[1] - ptra[1]) / diff;
+	tab[2] = (ptrb[2] - ptra[2]) / diff;
+	tab[3] = (ptrb[3] - ptra[3]) / diff;
+	ft_memcpy(&res, tab, 4);
 	return (res);
-	}*/
+}
 
 void				segment(t_env env, t_dot a, t_dot b)
 {
 	t_dot	tmp;
 	double	div;
-/*	int		col;
+	int		col;
 
-	col = color_add(a, b);*/
 	tmp.x = 0;
 	tmp.y = 0;
 	tmp.color = a.color;
 	(a.x > b.x ? ft_swap(&a.y, &b.y) : (void)0);
 	(a.x > b.x ? ft_swap(&a.x, &b.x) : (void)0);
+	col = color_add(a, b);
 	div = (double)(b.y - a.y) / (double)(b.x - a.x);
 	if (div >= -1 && div <= 1)
 		while (a.x + tmp.x <= b.x)
 		{
 			img_addr(env, a.x + tmp.x, a.y + tmp.y, tmp.color);
+			tmp.color += col;
 			tmp.x++;
 			tmp.y =  tmp.x * div;
 		}
@@ -101,6 +117,7 @@ void				segment(t_env env, t_dot a, t_dot b)
 		while (a.y + tmp.y != b.y)
 		{
 			img_addr(env, a.x + tmp.x, a.y + tmp.y, tmp.color);
+			tmp.color += col;
 			tmp.y = (a.y < b.y ? tmp.y + 1 : tmp.y - 1);
 			tmp.x = tmp.y / div;
 			if (a.y + tmp.y == b.y)
